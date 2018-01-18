5688e3024edd22291ec4c61ed40f60fd253c1646
曾佑森 <2356281422@qq.com>
2018-01-18 22:51:52 +0800

    数据保存

--- redux_music/src/containers/playInterface/playinterface.js
+++ redux_music/src/containers/playInterface/playinterface.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export default class PlayInterface extends React.Component{
+    // 播放界面需要永达的数据
+    // 已经缓冲的进度
+    // 已经播放的进度/当前时间
+    // 音乐总时长
+    // 音频的地址
+    // 音频的封面
+    // 音频的id
+    // 进度条长度
+    // ...
     render(){
         return(
             <div>interface</div>
